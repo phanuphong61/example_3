ac1ee01f344d1374e193fba419a4ba69b08912f2
PHDEV.MCM2 <phanuphong.du@gmail.com>
2024-05-17 11:36:01 +0700

    save - 3

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,53 +12,68 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Example 3"),
+        title: const Text("Flutter Layout Demo"),
       ),
-      body: const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            Column(
+            Image.network(
+                'https://images.unsplash.com/photo-1715586042479-256282ff3cc9?q=80&w=1000'),
+            const Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Title"),
+                      Text("Description"),
+                    ],
+                  ),
+                  Row(
+                    children: [Icon(Icons.star), Text("41")],
+                  )
+                ],
+              ),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Icon(Icons.call),
-                Text("Call"),
+                Column(
+                  children: [
+                    Icon(Icons.call),
+                    Text("Call"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.route),
+                    Text("Route"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.share),
+                    Text("Share"),
+                  ],
+                )
               ],
             ),
-            Column(
-              children: [
-                Icon(Icons.route),
-                Text("Route"),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(Icons.share),
-                Text("Share"),
-              ],
+            const Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    textAlign: TextAlign.justify,
+                  )
+                ],
+              ),
             )
-          ]),
+          ],
+        ),
+      ),
     );
   }
 }
-
-// class HomeScreen extends StatefulWidget {
-//   const HomeScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Row(
-//         children: [
-//           Column(
-//             children: [
-//               Icon(Icons.call),
-//               Text("Call"),
-//             ],
-//           ),
-//           Column(),
-//           Column()
-//         ]
-//       ),
-//     );
-//   }
-
-// }
